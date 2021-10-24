b638f97ba50c5a3606402ee919b286f14851353d
Ярослав <@DESKTOP-F85DT2O>
2021-10-24 19:29:00 +0200

    Добавьте файлы проекта.

---
+++ Hi_Neo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hi_Neo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now}");
+            Console.ReadLine();
+        }
+    }
+}
